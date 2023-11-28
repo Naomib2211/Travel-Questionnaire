4e96bc5f2429a80fec1f8cf3e3b548e30de5cea4
Naomib2211 <131289776+Naomib2211@users.noreply.github.com>
2023-11-28 10:03:38 +0000

    Add script file for js and further structure

---
+++ assets/js/script.js
@@ -0,0 +1,37 @@
+// start btn
+startBtn.addEventListener("click", startGame)
+
+// shuffle the questions
+function shuffleQuestions() {
+    questionsShuffled = questions.sort(() => Math.random() -0.5); 
+}
+
+// shuffle the options
+function shuffleOptions(options) {
+    optionsShuffled = options.sort(() => Math.random() - 0.5);
+
+}
+
+// start the game
+function startGame() {
+    welcome.classlist.add("hide");
+    startBtn.classlist.add("hide");
+    shuffleQuestions()
+    currentQuestions = 0;
+
+questionbox.classlist.remove("hide");
+    getNextQ();
+}
+
+//go to next question
+
+function getNextQ() {
+    displayQuestion(questionsShuffle[currentQuestion]);
+}
+
+// present next question and relating options
+
+function displayQuestion(q) {
+    // question
+    
+}
