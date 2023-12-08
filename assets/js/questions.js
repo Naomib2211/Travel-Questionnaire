eef08d4cef68b59b71a081df913134d138dc0a02
Naomib2211 <131289776+Naomib2211@users.noreply.github.com>
2023-12-08 01:12:40 +0000

    Add questions.js file to javascript folder

---
+++ assets/js/questions.js
@@ -0,0 +1,108 @@
+const questions = [{
+
+    question: "What type of shopping experience do you prefer?",
+    options: [{
+            text: "Vintage stores and thrift shops",
+            points: 1
+        },
+        {
+            text: "Luxury brands and trendy boutiques",
+            points: 2
+        },
+        {
+            text: "Unique local markets and artisan shops",
+            points: 3
+        },
+        {
+            text: "I'm not interested in shopping",
+            points: 4
+        }
+    ]
+},
+{
+        question: "How do you like to spend your evenings?",
+        options: [{
+            text: "Relaxing at a cozy café or pub", 
+            points: 1
+        },
+        {
+            text: "Dancing the night away at clubs and parties",
+            points: 2
+        },
+        {
+            text: "Attending cultural events and live performances",
+            points: 3 
+        },
+        {   
+            text: "Enjoying a leisurely dinner and strolling by the beach",
+            points: 4
+        }
+    ]
+},
+{
+        question: "Which landscape appeals to you the most?",
+        options: [{
+            text: "I enjoy a mix of different landscares",
+            points: 1
+        },
+        {
+            text: "Rolling hills and lush countryside",
+            points: 2
+        },
+        {
+            text: "Majestic mountains and stunning fjords",
+            points: 3
+        },
+        {
+            text: "Breathtaking beaches and crystal-clear waters",
+            points: 4
+        }
+    ]
+},
+{
+        question: "What kind of cuisine are you most interested in?",
+        options: [{
+            text: "I'm open to trying different cuisines",
+            points: 1
+        },
+        {
+            text: "Mediterranean dishes and seafood delicacies",
+            points: 2
+        },
+        {
+            text: "Nordic cuisine with fresg ingredients and seafood",
+            points: 3
+        },
+        {
+            text: "Traditional Greek or local delicicacies",
+            points: 4
+        },
+    ]
+},
+{
+        question: "What architectural style captivates you the most?",
+        options: [{
+            text: "I appreciate a mix of architectural styles",
+            points: 1
+        },
+        {
+            text: "Modern and innovative designs",
+            points: 2
+        },
+        {
+            text: "Quaint and charming historical buildings",
+            points: 3
+        },
+        {
+            text: "Viking-inspired structures and minimalist designs",
+            points: 4
+        }
+    ]
+},
+{
+        question: "Which outdoor activity excites you the most?",
+        options: [{
+            
+        }]
+}
+]
